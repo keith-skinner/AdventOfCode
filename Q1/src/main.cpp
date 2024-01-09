8a4ac039a30de0e9d83cfd6bb2f419ce3fc5b048
Keith Skinner <35318693+keith-skinner@users.noreply.github.com>
2024-01-09 04:59:52 +0000

    Q1P1

--- Q1/src/main.cpp
+++ Q1/src/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstdint>
 #include <algorithm>
 
-using std::cout;
-static constexpr auto endl = '\n';
+using namespace std;
 
 int main()
 {
-    cout << "Hello, Yall" << endl;
+    uint64_t line_num = 1;
+    uint64_t total = {};
+    std::ifstream input("input.txt");
+    std::string line;
+    while (std::getline(input, line))
+    {
+        constexpr auto isdigit = [](const char c) {return '0' <= c && c <= '9'; };
+        auto tens = find_if(begin(line), end(line), isdigit);
+        auto ones = find_if(rbegin(line), rend(line), isdigit);
+        if (tens != end(line) && ones != rend(line))
+        {
+            auto value = 10 * (*tens - '0') + (*ones - '0');
+            cout << line_num << ": " << value << endl;
+            total += value;
+        }
+        else cout << line_num << " no number" << endl;
+        ++line_num;
+    }
+
+    cout << total << endl;
 }
